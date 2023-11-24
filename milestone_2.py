6dfb285b388ff8cc9b85543d1f13edcf1a6ee96d
Your Name <jpigera45@gmail.com>
2023-11-24 15:15:18 +0000

    adding comments to the python file

--- milestone_2.py
+++ milestone_2.py
@@ -1,18 +1,20 @@
 import random 
 
-word_list = ["Apple", "Lemon", "Avocado", "Pear", "Pineapple"]
+word_list = ["Apple", "Lemon", "Avocado", "Pear", "Pineapple"] #A list of words to choose from
 
 print(word_list)
 
-word = random.choice(word_list)
+word = random.choice(word_list) #choosing a random word from the list and assigning it to the variable 'word'
 
 print(word)
 
-guess = input("Enter a single letter:")
+guess = input("Enter a single letter:") #asks the user for an input of one letter
 
 print(guess)
 
-if len(guess) == 1 and guess.isalpha() == True :
+
+
+if len(guess) == 1 and guess.isalpha() == True : #if statement checks that the input is one letter and the charachter is a letter from the alphabet
 
     print("Good guess!")
 
