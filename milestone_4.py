9e133312fd18a8929d7710d0d2f5aecd0bae3740
Your Name <jpigera45@gmail.com>
2023-11-27 11:58:45 +0000

    making a new milestone python project for milestone 4

---
+++ milestone_4.py
@@ -0,0 +1,67 @@
+import random 
+
+word_list = ["apple", "lemon", "avocado", "pear", "pineapple"] #a list of words to choose from
+
+
+print(word_list)
+
+word = random.choice(word_list) # generates random word from the list
+
+print(word)
+
+secret_word = list(word) # splits the word up into a list of letters
+
+print(secret_word)
+
+def ask_for_input() : # function that asks the user for the input and makes sure it is a valid input
+
+    guess = input("Guess a letter:")
+
+    while len(guess) != 1 or guess.isalpha() == False:
+
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+        guess = input("Guess a letter:")
+        
+        if guess.isalpha() == True and len(guess) == 1:
+
+            break
+
+    check_guess(guess) # check guess function is called as part of the ask_for_input() function
+
+def check_guess(guess) : # The function turns the input into lower case and then checks if the guess is in the secret word
+
+    guess = guess.lower()
+
+    if guess in secret_word :
+
+        print(f"Good guess! {guess} is in the word.")
+
+    else :
+
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+    return guess
+
+ask_for_input()
+
+class Hangman :
+
+    def __init__(self, word_list, num_lives= 5) :
+
+        self.word_list = word_list
+
+        self.num_lives = num_lives
+
+        self.word = word
+
+        self.word_guessed = wordguessed
+
+        self.num_letters = num_letters
+
+        self.num_lives = num_lives
+
+        self.list_of_guesses = list_of_guesses
+
+
+    
