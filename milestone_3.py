24815dc51d355403dbb1feab268fd68bfbf5440a
Your Name <jpigera45@gmail.com>
2023-11-27 10:03:50 +0000

    "updating the milestone_3 python file with update"

--- milestone_3.py
+++ milestone_3.py
@@ -1,19 +1,19 @@
 import random 
 
-word_list = ["apple", "lemon", "avocado", "pear", "pineapple"] #A list of words to choose from
+word_list = ["apple", "lemon", "avocado", "pear", "pineapple"] #a list of words to choose from
 
 
 print(word_list)
 
-word = random.choice(word_list)
+word = random.choice(word_list) # generates random word from the list
 
 print(word)
 
-secret_word = list(word)
+secret_word = list(word) # splits the word up into a list of letters
 
 print(secret_word)
 
-def ask_for_input() :
+def ask_for_input() : # function that asks the user for the input and makes sure it is a valid input
 
     guess = input("Guess a letter:")
 
@@ -22,14 +22,14 @@ def ask_for_input() :
         print("Invalid letter. Please, enter a single alphabetical character.")
 
         guess = input("Guess a letter:")
-
+        
         if guess.isalpha() == True and len(guess) == 1:
 
             break
 
-    check_guess(guess)
+    check_guess(guess) # check guess function is called as part of the ask_for_input() function
 
-def check_guess(guess) :
+def check_guess(guess) : # The function turns the input into lower case and then checks if the guess is in the secret word
 
     guess = guess.lower()
 
